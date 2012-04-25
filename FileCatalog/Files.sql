4bd1dd1e2b8f80ed6f48d9d5d8390c60cee1e4fc
Keivan Beigi <kay.one@gmail.com>
2012-04-25 14:55:22 -0700

    initial commit

---
+++ FileCatalog/Files.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Files](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Path] [varchar](1000) NOT NULL,
+	[Size] [bigint] NOT NULL,
+	[FileName] [varchar](50) NOT NULL
+) ON [PRIMARY]
---
+++ FileCatalog/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using PetaPoco;
+
+namespace FileCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection =
+                new SqlConnection(@"Data Source=.\SQLExpress;Database=FileCatalog; Integrated Security=True");
+
+
+            var database = new Database(connection, Database.DBType.SqlServer);
+
+            
+            var sourceDirectory = new DirectoryInfo(@"C:\Dropbox\Presentations\FileCatalog\Notepad++");
+
+
+            var files = sourceDirectory.GetFiles("*.*", SearchOption.AllDirectories)
+                  .Select(f => new CatalogedFile { FileName = f.Name, Path = f.FullName, Size = f.Length });
+
+
+            connection.Open();
+
+            foreach (var catalogedFile in files)
+            {
+                Console.WriteLine("Adding {0} to catalog.", catalogedFile.Path);
+                database.Insert(catalogedFile);
+            }
+
+            connection.Close();
+
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+
+    [TableName("Files")]
+    public class CatalogedFile
+    {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+        public long Size { get; set; }
+    }                                       
+}
